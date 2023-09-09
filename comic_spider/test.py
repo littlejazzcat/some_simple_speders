82fbc0419b4a1ba15ac0f48461003b4fdf02c178
Jazz cat <1348842185@qq.com>
2023-09-10 00:31:48 +0800

    添加测试案例

--- comic_spider/test.py
+++ comic_spider/test.py
@@ -12,21 +12,21 @@ def read_html():
             f.seek(0)
             html = f.read()
 
-        pic_pattern = re.compile('<div.*?style="background:url\((.*?)\)')#封面图地址
-        title_pattern = re.compile('<div style="padding:0px 5px 0px 5px;">.*?<a href=\'(?P<url>.*?)\'>(?P<title>.*?)</a> <br />.*?\
-                                   <font class="pagefoot">(?P<author>.*?)</font>')#漫画地址、标题和作者
-        score_pattern = re.compile('<p style=.*?<b>(.*?)</b>')#评分
+        pic_pattern = re.compile('<div[.\s]*style="background:url\((.*?)\)')#封面图地址
+        title_pattern = re.compile('<div style="padding:0px 5px 0px 5px;">[.\s]*<a href=\'(?P<url>.*?)\'.*?>(?P<title>.*?)</a> <br />.*?\
+                                   [.\s]*<font class="pagefoot">(?P<author>.*?)</font>')#漫画地址、标题和作者
+        score_pattern = re.compile('<p style=.*?[.\s]*.*?<b>(.*?)</b>')#评分
         
 
         img_urls = pic_pattern.findall(html,re.S)
         details = title_pattern.findall(html,re.S)
         scores = score_pattern.findall(html,re.S)
 
-        for score in scores:
-            print(f'{score}')
+        #for score in scores: #it is ok
+            #print(f'{score}')
         
-        for img_url in img_urls:
-            print(f'{img_url}')
+        #for img_url in img_urls:#it is ok
+            #print(f'{img_url}')
 
         for comic_url,title,author in details:
             print(f'{comic_url},{title},{author}')
